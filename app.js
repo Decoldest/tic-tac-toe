080cfda0487e98e4ba5a598f52f372f65238d1bb
Ryan Wong <ryanjameswong@outlook.com>
2024-01-19 14:28:15 -0500

    Include game control factory function to add players and set round

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-let gridSpace = 'temp';
-
 const gameBoard = (function() {
   const columns = 3;
   const rows = 3;
-  const board = new Array(rows).fill(null).map(() => new Array(columns).fill(null).map(() => gridSpace()));
-  
-  const getBoard = () => board;
+  const boardSpaces = Array.from({ length: rows }, () =>
+    Array.from({ length: columns }, () => gridSpace())
+  );
+
+  const getBoard = () => boardSpaces;
 
   const addMoveToBoard = (player, positionX, positionY) => {
-    const checkEmptySpace = board[positionX][positionY].getValue();
+    const checkEmptySpace = boardSpaces[positionX][positionY].getValue();
     if (checkEmptySpace) {
       return;
     } else {
-      board[positionX][positionY].addPlayerToSpace(player);
+      boardSpaces[positionX][positionY].addPlayerToSpace(player);
     }
-  }
+  };
 
   const printBoard = () => {
-    const boardWithCellValues = board.map((row) => row.map((gridSpace) => gridSpace.getValue()));
+    const boardWithCellValues = boardSpaces.map((row) => row.map((gridSpace) => gridSpace.getValue()));
     console.log(boardWithCellValues);
-  }
+  };
 
   function gridSpace() {
     let gridValue = null;
@@ -29,17 +29,41 @@ const gameBoard = (function() {
     const addPlayerToSpace = (player) => gridValue = player;
   
     return { addPlayerToSpace, getValue };
-  }
+  };
 
   return { addMoveToBoard, getBoard, printBoard };
 })();
 
+console.log(gameBoard.addMoveToBoard('x',1,1));
+console.log(gameBoard.printBoard());
 
-function createPlayer (piece) {
-  return { piece };
+
+function gameControl() {
+
+  const board = gameBoard;
+  players = [];
+
+  function createPlayer (piece) {
+    return { piece };
+  }
+
+  const getControllerBoard = () => {
+    board.getBoard;
+  }
+
+  const addPlayersToGame = () => {
+    players = [createPlayer('X'), createPlayer('O')]
+  }
+  
+  let currentPlayer = players[0];
+
+  const switchCurrentPlayer = () => {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+  }
+
+  const playerTurn = (player, x, y) => {
+    board.addMoveToBoard(currentPlayer, x, y);
+  };
+
+  return { switchCurrentPlayer, playerTurn, getControllerBoard };
 }
-
-gameBoard.printBoard();
-gameBoard.addMoveToBoard('x', 1, 1);
-gameBoard.addMoveToBoard('y', 1, 1);
-gameBoard.printBoard();
