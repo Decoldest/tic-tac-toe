e4e47e6bdc5633b4d5188e45d430229bb3430abe
Ryan Wong <ryanjameswong@outlook.com>
2024-01-19 17:09:57 -0500

    Check win condition for each row win

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const gameBoard = (function() {
     Array.from({ length: columns }, () => gridSpace())
   );
 
-  const getBoard = () => boardSpaces;
-
   const addMoveToBoard = (player, positionX, positionY) => {
     const checkEmptySpace = boardSpaces[positionX][positionY].getValue();
     if (checkEmptySpace) {
@@ -16,9 +14,9 @@ const gameBoard = (function() {
     }
   };
 
-  const printBoard = () => {
+  const getBoard = () => {
     const boardWithCellValues = boardSpaces.map((row) => row.map((gridSpace) => gridSpace.getValue()));
-    console.log(boardWithCellValues);
+    return boardWithCellValues;
   };
 
   function gridSpace() {
@@ -31,7 +29,7 @@ const gameBoard = (function() {
     return { addPlayerToSpace, getValue };
   };
 
-  return { addMoveToBoard, getBoard, printBoard };
+  return { addMoveToBoard, getBoard };
 })();
 
 // gameBoard.addMoveToBoard('x', 1, 1);
@@ -42,6 +40,7 @@ function gameControl() {
 
   const board = gameBoard;
   let players = [];
+  let currentPlayer
 
   function createPlayer (name, piece) {
     return { name, piece };
@@ -50,31 +49,43 @@ function gameControl() {
   const addPlayersToGame = (playersData) => {
     players = playersData.map(player => createPlayer(player.name, player.piece));
     console.log(`players are ${players[0].piece} ${players[1].piece}`);
-    console.log(players[0]);
+    currentPlayer = players[0];
   }
-  
-  let currentPlayer = players[0];
 
   const switchCurrentPlayer = () => {
     currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
   }
 
   const playerTurn = (x, y) => {
+    console.log(currentPlayer);
     board.addMoveToBoard(currentPlayer, x, y);
 
-    //checkWinner();
+    checkWinner();
 
     switchCurrentPlayer();
-    board.printBoard();
+    console.log(board.getBoard());
   };
+
+  function checkWinner () {
+    const currentBoard = board.getBoard();
+    //Returns true if all row values are equal (win)
+    const checkRow = row => row.every(val => val !== null && val === row[0]);    
+
+    for(let i = 0; i < currentBoard.length; i++){
+      console.log(checkRow(currentBoard[i]));
+    }
+
+    console.log("checking winner");
+    console.log(board.getBoard());
+  }
 
   return { addPlayersToGame, switchCurrentPlayer, playerTurn };
 }
 
-gameControl().addPlayersToGame([
-  { name: 'John', piece: 'X' },
-  { name: 'Jane', piece: 'O' }
-]);
+const game = gameControl();
 
-gameControl().playerTurn(2, 1)
-//gameControl().checkWinner();
+game.addPlayersToGame([
+  { name: 'Player 1', piece: 'X' },
+  { name: 'Player 2', piece: 'O' }
+]);
+game.playerTurn(2, 1);
